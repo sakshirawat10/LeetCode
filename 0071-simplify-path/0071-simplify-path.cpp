d0449a68832d236702d562bc244e2cba711dec7a
Sakshi Rawat <sonasakshi10@gmail.com>
2026-09-23 23:33:20 +0530

    Time: 6 ms (24.89%), Space: 10.9 MB (83.83%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+     vector<string>st;
+     string temp;
+     for(int i = 0;i<=path.size();i++){
+        if(i==path.size()||path[i]=='/'){
+            if(temp==".."){
+                if(!st.empty()){
+                    st.pop_back();
+                }
+            }
+            else if(temp!=""&&temp!="."){
+                st.push_back(temp);
+            }
+            temp="";
+        }
+        else{
+            temp+=path[i];
+        }
+     }   
+     string ans ="";
+     for(string dir:st){
+        ans +="/"+dir;
+     }
+     if(ans=="")
+     return "/";
+    return ans;
+    }
+};
