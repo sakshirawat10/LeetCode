21d4a2e7c0eee028515032760e18a0e0c14e1a56
Sakshi Rawat <sonasakshi10@gmail.com>
2026-09-26 22:29:50 +0530

    Time: 4 ms (48.35%), Space: 24.5 MB (40.51%) - LeetHub

---
+++ 0641-design-circular-deque/0641-design-circular-deque.cpp
@@ -0,0 +1,90 @@
+class MyCircularDeque {
+public:
+vector<int>dq;
+int front,rear,size,capacity;
+    MyCircularDeque(int k) {
+        capacity = k;
+        dq.resize(k);
+        front = 0;
+        rear = -1;
+        size = 0;
+    }
+    
+    bool insertFront(int value) {
+        if(isFull()){
+        return false;
+        }
+        front=(front-1+capacity)%capacity;
+        dq[front]=value;
+        if(size==0)
+        rear=front;
+        size++;
+        return true;
+    }
+    
+    bool insertLast(int value) {
+        if(isFull()){
+        return false;
+        }
+        rear=(rear+1)%capacity;
+        dq[rear]=value;
+        if(size==0)
+        front=rear;
+        size++;
+        return true;
+    }
+    
+    bool deleteFront() {
+        if(isEmpty()){
+            return false;
+        }
+        front=(front+1)%capacity;
+        size--;
+        return true;
+
+    }
+    
+    bool deleteLast() {
+        if(isEmpty()){
+            return false;
+        }
+        rear=(rear-1+capacity)%capacity;
+        size--;
+        return true;
+    }
+    
+    int getFront() {
+        if(isEmpty()){
+            return -1;
+        }
+        return dq[front];
+    }
+    
+    int getRear() {
+        if(isEmpty()){
+            return -1;
+        }
+        return dq[rear];
+    }
+    
+    bool isEmpty() {
+        return size==0;
+    }
+    
+    bool isFull() {
+        return size==capacity;
+    }
+};
+
+/**
+ * Your MyCircularDeque object will be instantiated and called as such:
+ * MyCircularDeque* obj = new MyCircularDeque(k);
+ * bool param_1 = obj->insertFront(value);
+ * bool param_2 = obj->insertLast(value);
+ * bool param_3 = obj->deleteFront();
+ * bool param_4 = obj->deleteLast();
+ * int param_5 = obj->getFront();
+ * int param_6 = obj->getRear();
+ * bool param_7 = obj->isEmpty();
+ * bool param_8 = obj->isFull();
+ */
